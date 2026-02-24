1521fec04f620a252bee04e9952823cae7aec704
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-24 22:41:07 +0530

    set the sensor route file to the index file

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const db = require("./firebase/firebase");
+const sensorRoutes = require("./routes/sensor")
 
 const app = express();
 
@@ -11,26 +11,7 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-app.post("/sensor-data", async (req, res) => {
-  try {
-    const data = req.body;
-    const id = Date.now().toString();
-
-    await db.ref("raw-readings/" + id).set({
-      ...data,
-      timestamp: Date.now(),
-    });
-
-    res.json({
-      message: "Saved to Firebase",
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      message: "Something went wrong",
-    });
-  }
-});
+app.use("/",sensorRoutes);
 
 app.listen(5000, () => {
   console.log("Server Running on Port 5000");
