b87b1f23a36bd3d9cebeb89318efdc5014f71f30
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-24 20:09:48 +0530

    add firebase connection

---
+++ backend/firebase/firebase.js
@@ -0,0 +1,11 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("../serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://iot-water-quality-73b9e-default-rtdb.asia-southeast1.firebasedatabase.app/",
+});
+
+const db = admin.database();
+
+module.exports = db;
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("./firebase/firebase");
 
 const app = express();
 
@@ -10,11 +11,25 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-app.post("/sensor-data", (req, res) => {
-  console.log("Received: " + req.body);
-  res.json({
-    message: "Data Received Successfully",
-  });
+app.post("/sensor-data", async (req, res) => {
+  try {
+    const data = req.body;
+    const id = Date.now().toString();
+
+    await db.ref("raw-readings/" + id).set({
+      ...data,
+      timestamp: Date.now(),
+    });
+
+    res.json({
+      message: "Saved to Firebase",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
 });
 
 app.listen(5000, () => {
