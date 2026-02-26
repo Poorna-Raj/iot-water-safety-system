8671ea7b02e8ef565139911ccdc5e9b980f1ac9b
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-02-26 19:08:23 +0530

    define the controller methods in the route files

--- backend/routes/sensor.js
+++ backend/routes/sensor.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const sensorController = require("../controllers/SensorController");
 
 router.post("/sensor-data", sensorController.saveSensorData);
+router.get("/last-readings", sensorController.getLastReadings);
+router.get("/weekly-averages", sensorController.getWeeklyAverages);
 
 module.exports = router;
