ad0137cd23b70b4bb147b5e1ef96a33c823f48bb
Poorna Rajapaksha <poornarajapaksha339@gmail.com>
2026-03-09 17:08:20 +0530

    add firebase configuration data

--- firmware/Display_Code/display.ino/display.ino.ino
+++ firmware/Display_Code/display.ino/display.ino.ino
@@ -1,35 +1,36 @@
-#include<ESP8266WiFi.h>
-#include<Firebase_ESP_Client.h>
-#include<LiquidCrystal_I2C.h>
+#include <ESP8266WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 
+#define WIFI_SSID "A13Pro";
+#define WIFI_PASSWORD "poorna4449";
 
-#define WIFI_SSID "";
-#define WIFI_PASSWORD "";
-
-#define API_KEY "";
-#define DATABASE_URL "";
+#define API_KEY "AIzaSyDo5ZXbFZ6pT1_kH0BqFfMxqmRC4ct4cuk";
+#define DATABASE_URL                                                           \
+  "https://"                                                                   \
+  "iot-water-quality-73b9e-default-rtdb.asia-southeast1.firebasedatabase.app";
 
 #define IR_SENSOR D5;
-#define  BUZZER D6;
+#define BUZZER D6;
 
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 
-LiquidCrystal_I2C lcd(0x27,17,2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 bool safeStatus = true;
 String messageText = "";
-int qaulityScore =0;
+int qaulityScore = 0;
 
 void streamCallBack(FirebaseStream data);
 void streamTimeoutCallBack(bool timeout);
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
 
-  pinMode(IR_SENSOR,INPUT);
+  pinMode(IR_SENSOR, INPUT);
   pinMode(BUZZER, OUTPUT);
 
   lcd.init();
@@ -38,12 +39,11 @@ void setup()
   WiFi.begin(WiFi_SSID, WiFi_PASSWORD);
   Serial.print("Connecting to WiFi");
 
-  while(WiFi.status() !=WL_CONNECTED)
-  {
-    delay(500)
-    Serial.print(".");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500) Serial.print(".");
   }
 
+  Serial.println();
   Serial.print("Connected");
 
   config.api_key = API_KEY;
@@ -52,66 +52,55 @@ void setup()
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
 
-  if(!Firebase.RTDB.beginStream(&fbdo, "/current-status"))
-  {
+  if (!Firebase.RTDB.beginStream(&fbdo, "/current-status")) {
     Serial.println("Stream got Failed");
     Serial.println(fbdo.errorReason());
   }
 
   Firebase.RTDB.setStreamCallBack(&fbdo, streamCallBack, streamTimeoutCallBack);
-  
 }
 
-void loop()
-{
+void loop() {
   int irValue = digitalRead(IR_SENSOR);
 
-  if(!safeStatus && irValue == HIGH)
-  {
+  if (!safeStatus && irValue == HIGH) {
     digitalWrite(BUZZER, HIGH);
-  }
-  else
-  {
+  } else {
     digitalWrite(BUZZER, LOW);
   }
 
   delay(100);
 }
- void setStreamCallBack(FirebaseStream data)
-  {
-    Serial.println("Data Updated");
+void setStreamCallBack(FirebaseStream data) {
+  Serial.println("Data Updated");
 
-    if(data.dataType()=="json")
-    {
-      FirebaseJson *json = data.to<FirebaseJson *>();
+  if (data.dataType() == "json") {
+    FirebaseJson *json = data.to<FirebaseJson *>();
 
-      FirebaseJsonData result;
+    FirebaseJsonData result;
 
-      json->get(result, "safe");
-      safeStatus = result.boolValue;
+    json->get(result, "safe");
+    safeStatus = result.boolValue;
 
-      json->get(result, "message");
-      messageText = result.stringValue;
+    json->get(result, "message");
+    messageText = result.stringValue;
 
-      json->get(result, "qualityScore");
-      qualityScore = result.intValue;
+    json->get(result, "qualityScore");
+    qualityScore = result.intValue;
 
-      lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print("Score:");
-      lcd.print(quantiyScore);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Score:");
+    lcd.print(qualityScore);
 
-      lcd.setCursor(0,1);
-      lcd.print(messageText);
+    lcd.setCursor(0, 1);
+    lcd.print(messageText);
 
-      Serial.println(messageText);
-
-    }
+    Serial.println(messageText);
   }
-void streamTimeOutCallBack(bool timeout)
-{
-  if(timeout)
-  {
+}
+void streamTimeOutCallBack(bool timeout) {
+  if (timeout) {
     Serial.println("Stream Timeout, reconnecting");
   }
 }
